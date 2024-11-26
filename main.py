1339df47b87d6db3ab8e3c0a431e1056738ea017
Jose Germán Martínez <jgermanmm@gmail.com>
2024-11-26 15:29:43 +0000

    Primer commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# Es un comentario y esa línea no se será ejecutada
+
+#lee la variable, le asigna un valor y la guarda en memoria
+nombre = input("Ingresa tu nombre: ") # imnprime un mensaje al usuario y espera que ingrese un dato
+apellido = input("Ingresa tu apellido: ")
+ciudad = input("Ingresa tu ciudad: ")
+
+
+print(f"Hola, {nombre} {apellido}. Seleccionaste la ciudad: {ciudad}.")
+
+
+nombre_completo = f"{nombre} {apellido}"
+
+print(nombre_completo)
